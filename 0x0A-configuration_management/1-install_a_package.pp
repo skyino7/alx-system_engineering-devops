cb00b3f9c29099e2328da40fedb563b135b1ff9b
Seth Ketsi <christambassador12@gmail.com>
2023-12-22 07:08:00 -0800

    updated

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,25 @@
 # Using Puppet, install flask from pip3
+
+# Install Python 3.8.10
+package { 'python3.8':
+  ensure => present,
+}
+
+# Install pip
+package { 'python3-pip':
+  ensure => present,
+}
+
+# Install Flask 2.1.0
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip',
+  require  => Package['python3-pip'],
+}
+
+# Install Werkzeug 2.1.1
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip',
+  require  => Package['python3-pip'],
 }

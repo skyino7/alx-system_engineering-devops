ae29b9f66ff9cf394ea962c053a315c5a453003f
Seth Ketsi <christambassador12@gmail.com>
2023-12-26 18:00:49 +0000

    Puppet to make changes to our configuration file.

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,11 @@
 #Puppet to make changes to our configuration file.
-File_line { 'Turn off passwd auth':
+file_line { 'Turn off passwd auth':
   ensure => present,
   line   => 'PasswordAuthentication no',
   path   => '/etc/ssh/ssh_config',
 }
 
-File_line { 'Declare identity file':
+file_line { 'Declare identity file':
   ensure => present,
   line   => '/etc/ssh/ssh_config',
   path   => 'IdentityFile ~/.ssh/school'

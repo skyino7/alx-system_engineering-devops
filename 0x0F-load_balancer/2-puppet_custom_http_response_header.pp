6334d7a545edaa07aea802a18b73373e19ebeb79
Seth Ketsi <christambassador12@gmail.com>
2024-01-09 08:38:52 +0000

    Install and configure HAproxy on your lb-01 server

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,22 +1,31 @@
-# Add a custom HTTP header with Puppet
-exec {'update':
-  command => '/usr/bin/apt-get update'
+exec {'update_system':
+  command => '/usr/bin/apt-get -y update',
 }
 
 package {'nginx':
-  ensure => installed
+  ensure => 'installed',
 }
 
-file_line {'header':
-  ensure => present,
-  path  => '/etc/nginx/sites-available/default'
-  line  => "server \tadd_header X-Served-By ${hostname};",
-  after => 'server_name _;',
-  require => Service['nginx']
+package { 'augeas-tools':
+  ensure => 'installed',
 }
 
-service { 'nginx':
-  ensure => 'running',
-  enable => true,
+
+service {'nginx':
+  ensure  => 'running',
+  enable  => true,
   require => Package['nginx'],
+}
+
+file {'/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+# Use Augeas to add the custom header
+augeas { 'nginx_custom_header':
+  context => '/files/etc/nginx/sites-available/default',
+  changes => [
+    "set server[last()]/location[last()]/add_header X-Served-By ${hostname}",
+  ],
+  require => [Package['nginx'], Service['nginx']],
 }

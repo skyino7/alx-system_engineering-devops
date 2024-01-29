cac0b3dfccf1373566cbde554dd5582e572b8965
Seth Ketsi <christambassador12@gmail.com>
2024-01-29 17:15:59 +0000

    updated

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""script that, using this REST API, for a given employee ID,
+"""Script that, using this REST API, for a given employee ID,
 returns information about his/her TODO list progress."""
 
 import sys

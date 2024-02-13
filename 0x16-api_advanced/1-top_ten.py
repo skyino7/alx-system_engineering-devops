061245ba708b1038fa395b877bf32412fd33e289
Seth Ketsi <christambassador12@gmail.com>
2024-02-13 12:34:54 +0000

    Task 1 & 2 updated

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,7 +9,7 @@ def top_ten(subreddit):
     """Function to query the Reddit API and print the titles of the
     first 10 hot posts listed for a given subreddit"""
 
-    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
 

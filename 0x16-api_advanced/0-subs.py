f68bd38d7d76b8839e833c1156eebeffa7c1eee4
Seth Ketsi <christambassador12@gmail.com>
2024-02-14 09:00:45 +0000

    updated

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ def number_of_subscribers(subreddit):
     """Function to query the Reddit API and
     returns the number of subscribers"""
 
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
 

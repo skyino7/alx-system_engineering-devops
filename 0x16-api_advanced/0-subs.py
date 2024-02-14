9a3a8a18c93936a0ff19cdeb71436fa49fa41d92
Seth Ketsi <christambassador12@gmail.com>
2024-02-14 09:06:37 +0000

    updated

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,6 +11,12 @@ def number_of_subscribers(subreddit):
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
-    if response.status_code != 200:
+    if response.status_code == 200:
+        data = response.json()
+        if 'data' in data and 'subscribers' in data['data']:
+            subscribers = data['data']['subscribers']
+            return subscribers
+        else:
+            return 0
+    else:
         return 0
-    return response.json().get('data').get('subscribers')

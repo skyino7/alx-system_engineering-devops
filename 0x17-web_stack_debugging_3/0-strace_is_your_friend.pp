1ef80acb78e95ba6f4a5925197a5a25380ab4752
Seth Ketsi <christambassador12@gmail.com>
2024-02-15 08:51:44 +0000

    Strace is your friend

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,11 @@
+#Puppet
+exec { 'replace_extension':
+  command => '/usr/bin/sed -i "s/.phpp/.php/g" /var/www/html/wp-settings.php',
+  path    => '/usr/bin:/usr/sbin:/bin',
+  notify  => Exec['restart_apache_service'],
+}
+
+exec { 'restart_apache_service':
+  command     => '/usr/sbin/service apache2 restart',
+  refreshonly => true,
+}
